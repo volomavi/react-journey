bd08a264f881856fd2dec29db1185899d047e263
Ivan Bogomolov <volomavi@hotmail.com>
2022-05-26 14:17:16 -0700

    props

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,21 +1,18 @@
 import React from "react";
-import Header from './Header'
-import Order from './Order'
-import Inventory from './Inventory'
-
-
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
 
 class App extends React.Component {
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header/>
-            
+          <Header tagline="Fresh Seafood Market" />
         </div>
-        <Order/>
+        <Order />
 
-        <Inventory/>
+        <Inventory />
       </div>
     );
   }
