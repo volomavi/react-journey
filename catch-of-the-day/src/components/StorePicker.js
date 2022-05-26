786adabe9b8e5fc9857429b2ff79496ead2c51da
Ivan Bogomolov <volomavi@hotmail.com>
2022-05-26 12:47:16 -0700

    prettier goes a long way

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 
 class StorePicker extends React.Component {
   render() {
     return (
       <>
         <p>fish!</p>
-      <form action="" className="store-selector">
-        <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder='Store Name' />
-        <button type='submit'>Visit Store</button>
-      </form>
+        <form action="" className="store-selector">
+          <h2>Please Enter A Store</h2>
+          <input type="text" required placeholder="Store Name" />
+          <button type="submit">Visit Store</button>
+        </form>
       </>
-      )
+    );
   }
 }
 
