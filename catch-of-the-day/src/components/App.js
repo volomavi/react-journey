ce9de128c14945d766f30bf6315e85fb3abad2d8
Ivan Bogomolov <volomavi@hotmail.com>
2022-05-31 22:19:46 -0700

    updating state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -23,6 +23,15 @@ class App extends React.Component {
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
+
+  addToOrder = (key) => {
+    //take a copy of state
+    const oder = { ...this.state.order };
+    // add or order or update
+    order[key] = order[key] + 1 || 1;
+    //call setstate to update our state object
+    this.setState({ order: order });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -30,7 +39,12 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map((key) => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
