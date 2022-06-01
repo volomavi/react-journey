7020bce26394c2756e52429e76e91a370fdfb228
Ivan Bogomolov <volomavi@hotmail.com>
2022-05-31 21:22:11 -0700

    united States of React

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,20 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {},
+  };
+  addFish = (fish) => {
+    //take a copy of the existing state to prevent mutation
+    const fishes = { ...this.state.fishes };
+    //add our new fish to that fishes var
+    fishes[`fish${Date.now()}`] = fish;
+    //set new fishes object to state
+    this.setState({
+      fishes: fishes,
+    });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -12,7 +26,7 @@ class App extends React.Component {
         </div>
         <Order />
 
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
