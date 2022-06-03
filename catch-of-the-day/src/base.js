077a7070cdad74880809c646a1f2a314e247ec12
Ivan Bogomolov <volomavi@hotmail.com>
2022-06-02 23:24:10 -0700

    firebase setup

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,14 @@
+import Rebase from 're-base'
+import firebase from 'firebase'
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "keepItSecret,keepItSafe",
+  authDomain: "cotd-71d47.firebaseapp.com",
+  databaseURL: "https://cotd-71d47-default-rtdb.firebaseio.com"
+})
+
+const base = Rebase.createClass(firebaseApp.database())
+
+export {firebaseApp}
+
+export default base;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,12 +4,24 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
+import base from "../base";
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {},
   };
+  componentDidMount() {
+    const { params } = this.props.match;
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes",
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
   addFish = (fish) => {
     //take a copy of the existing state to prevent mutation
     const fishes = { ...this.state.fishes };
