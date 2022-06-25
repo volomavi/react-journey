27b2df4bd5e2a67f8d5a94cf9569e69cf9161c2a
Ivan Bogomolov <volomavi@hotmail.com>
2022-06-25 13:01:02 -0700

    keep it safe

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,7 +2,7 @@ import Rebase from 're-base'
 import firebase from 'firebase'
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "secret",
+  apiKey: `${REACT_FB_SECRET}`,
   authDomain: "cotd-71d47.firebaseapp.com",
   databaseURL: "https://cotd-71d47-default-rtdb.firebaseio.com"
 })
